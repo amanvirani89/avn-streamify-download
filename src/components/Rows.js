06d7814c31958527167c30f5f3515b8a012355dc
amanvirani <amanvirani896@gmail.com>
2021-04-09 18:02:19 -0400

    Added subscription via stripe extension and added movie trailer

--- src/components/Rows.js
+++ src/components/Rows.js
@@ -1,9 +1,12 @@
 import axios from '../axios';
 import React, { useEffect, useState } from 'react';
 import '../css/Rows.css';
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
 
 function Rows({ movieTitle, fetchUrl }) {
   const [Movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     async function movieData() {
@@ -13,7 +16,35 @@ function Rows({ movieTitle, fetchUrl }) {
     }
 
     movieData();
-  }, []);
+  }, [fetchUrl]);
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      console.log(movie);
+      movieTrailer(
+        movie.name || movie.title || movie.original_name || movie.original_title
+      )
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+          console.log(urlParams);
+          console.log(trailerUrl);
+          console.log(url);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   console.log(Movies);
 
   return (
@@ -25,24 +56,53 @@ function Rows({ movieTitle, fetchUrl }) {
         {Movies.map(
           (movie) =>
             movie.poster_path && (
-              <img
-                key={movie.id}
-                src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt={
-                  movie.title ||
-                  movie.original_title ||
-                  movie.name ||
-                  movie.original_name
-                }
-              />
+              <div className="movie-card">
+                <div className="card-container">
+                  <img
+                    key={movie.id}
+                    onClick={() => handleClick(movie)}
+                    src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={
+                      movie.title ||
+                      movie.original_title ||
+                      movie.name ||
+                      movie.original_name
+                    }
+                  />
+                  {/* {console.log(movie)} */}
+
+                  {/* <div className="movie-over">
+                    <div className="over-text">
+                      <h2>Overview</h2>
+                      <span>{movie.overview}</span>
+                    </div>
+                  </div> */}
+                </div>
+
+                <div className="movie-description">
+                  <div>
+                    {' '}
+                    {movie.title ||
+                      movie.original_title ||
+                      movie.name ||
+                      movie.original_name}
+                  </div>
+                  <div>
+                    <span
+                      className={` ${
+                        movie.vote_average > 7 ? 'voting-green' : 'voting-red'
+                      } voting`}
+                    >
+                      {movie.vote_average}
+                    </span>
+                  </div>
+                </div>
+              </div>
             )
         )}
       </div>
-      {/* <div className="movie-data">
-        {Movies.map((movie) => (
-          <h2>{movie.vote_average}</h2>
-        ))}
-      </div> */}
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}></YouTube>}
     </div>
   );
 }
